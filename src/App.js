47e5125a78d2ca01dbc235869c7cf64b717906ae
Jyothish Kumar <JyothishElanjimattom@gmail.com>
2022-08-21 11:04:00 +0530

    Displayed movies row wise

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
 import './App.css'
 import Banner from "./Components/NavBar/Banner/Banner";
+import Rowpost from "./Components/RowPost/RowPost";
+
 
 function App() {
   return (
     <div className="App">
        <NavBar/>
       <Banner/>
+      <Rowpost/>
     </div>
    
   );
