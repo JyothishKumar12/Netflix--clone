9841b7e55ceee04366d96de4a6e486289be70be6
Jyothish Kumar <JyothishElanjimattom@gmail.com>
2022-08-21 09:36:43 +0530

    set navbar added logo and avatar

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavBar.css'
+function NavBar() {
+  return (
+    <div className='navbar'>
+        <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix logo"/> 
+        <img className="avatar" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="avatar logo"/>
+    </div>
+  )
+}
+
+export default NavBar
